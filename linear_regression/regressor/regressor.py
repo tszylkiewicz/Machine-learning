8e1cbcaeeabd58b2342f4bdcfdd1b0318a0ca900
tszylkiewicz <43911837+tszylkiewicz@users.noreply.github.com>
2020-12-21 01:13:51 +0100

    Update regressor.py

--- linear_regression/regressor/regressor.py
+++ linear_regression/regressor/regressor.py
@@ -1,7 +1,16 @@
+import os
 import sys
 import math
 import random
 import argparse
+
+
+K_FOLDS = 8
+LEARNING_RATE = 0.1
+MAX_STOP_CONDITION = 100
+MOMENTUM = 0.9
+N_EPOCH = 5000
+HIDDEN_NEURONS = [2, 3, 4, 6, 8, 10, 20]
 
 
 def normalize_value(value, min_value, max_value):
@@ -30,53 +39,23 @@ def calculate_column_ranges(data):
 def normalize_data(data, min_values, max_values):
     for i in range(len(data[0])):
         for j in range(len(data)):
-            data[j][i] = normalize_value(
-                data[j][i], min_values[i], max_values[i])
+            data[j][i] = normalize_value(data[j][i], min_values[i], max_values[i])
 
     return data
 
 
 def generate_folds(data, k_folds):
     random.shuffle(data)
+    return [data[i::k_folds] for i in range(k_folds)]
+    # folds = []
+    # avg = len(data) / float(k_folds)
+    # last = 0.0
+    
+    # while last < len(data):
+    #     folds.append(data[int(last):int(last + avg)])
+    #     last += avg
 
-    avg = len(data) / float(k_folds)
-    last = 0.0
-
-    folds = []
-
-    while last < len(data):
-        folds.append(data[int(last):int(last + avg)])
-        last += avg
-
-    return folds
-
-
-def generate_inputs_outputs(folds, k_folds):
-    training_inputs = []
-    training_outputs = []
-    validation_outputs = []
-
-    for fold in folds:
-        validation_fold_outputs = []
-        for x in fold:
-            validation_fold_outputs.append(x.pop())
-        validation_outputs.append(validation_fold_outputs)
-
-    validation_inputs = folds
-
-    for i in range(k_folds):
-        training_fold_inputs = []
-        training_fold_outputs = []
-
-        for j in range(k_folds):
-            if i == j:
-                continue
-            training_fold_inputs += validation_inputs[j]
-            training_fold_outputs += validation_outputs[j]
-        training_inputs.append(training_fold_inputs)
-        training_outputs.append(training_fold_outputs)
-
-    return training_inputs, training_outputs, validation_inputs, validation_outputs
+    # return folds
 
 
 def sigmoid(x):
@@ -87,179 +66,148 @@ def derivative_sigmoid(x):
     return x * (1 - x)
 
 
-def m_dot_v_v(x, y):
-    return sum([x[i] * y[i] for i in range(len(y))])
-
-# def activate(inputs, weights):
-#     activation = weights[-1]
-#     for i in range(len(weights) - 1):
-# 		activation += weights[i] * inputs[i]
-# 	return activation
+def activate(inputs, weights):
+    activation = weights[-1]
+    for i in range(len(weights) - 1):
+        activation += weights[i] * inputs[i]
+    return activation
 
 
+def train(training_set, weights_ih, weights_ho, n_inputs, n_hidden):
+    values = []
 
-def train(training_set, weights_ih, weights_ih_delta, weights_ho, weights_ho_delta, n_inputs, n_hidden, momentum, learning_rate):   
-    hidden_values = [0 for i in range(n_hidden)]
+    for input_values in training_set:
+        for hidden_index in range(n_hidden):
+            values.append(sigmoid(activate(input_values, weights_ih["weights"][hidden_index])))
 
-    for sample in range(len(training_set)):
-        input_values = training_set[sample]
-
-        for node in range(n_hidden):
-            hidden_values[node] = m_dot_v_v(
-                input_values[:n_inputs], weights_ih[node][:n_hidden])
-
-            hidden_values[node] = sigmoid(
-                hidden_values[node] + weights_ih[node][-1])
-
-        output_value = m_dot_v_v(hidden_values, weights_ho[:n_hidden])
-        output_value += weights_ho[-1]
-
+        output_value = activate(values, weights_ho["weights"])
         hidden_gradient = output_value - input_values[-1]
 
-        for hidden_node in range(n_hidden):
-            input_gradient = hidden_gradient * weights_ho[hidden_node] * derivative_sigmoid(hidden_values[hidden_node])
+        for hidden_index in range(n_hidden):
+            input_gradient = hidden_gradient * weights_ho["weights"][hidden_index] * derivative_sigmoid(values[hidden_index])
 
-            for input_node in range(n_inputs):
-                prev_in_delta = weights_ih_delta[hidden_node][input_node]
-                current_in_delta = -learning_rate * input_gradient * input_values[input_node]
-                weights_ih_delta[hidden_node][input_node] = current_in_delta
-                weights_ih[hidden_node][input_node] += current_in_delta + (momentum * prev_in_delta)
-
-            prev_in_delta = weights_ih_delta[hidden_node][-1]
-            current_in_delta = -learning_rate * input_gradient
-            weights_ih_delta[hidden_node][-1] = current_in_delta
-            weights_ih[hidden_node][-1] += current_in_delta + (momentum * prev_in_delta)
-
-            prev_hid_delta = weights_ho_delta[hidden_node]
-            current_hid_delta = -learning_rate * hidden_gradient * hidden_values[hidden_node]
-            weights_ho_delta[hidden_node] = current_hid_delta
-            weights_ho[hidden_node] += current_hid_delta + (momentum * prev_hid_delta)
-
-        prev_hid_delta = weights_ho_delta[-1]
-        current_hid_delta = -learning_rate * hidden_gradient
-        weights_ho_delta[-1] = current_hid_delta
-        weights_ho[-1] += current_hid_delta + (momentum * prev_hid_delta)
+            for input_index in range(n_inputs + 1):
+                if input_index == n_inputs:
+                    delta = -LEARNING_RATE * input_gradient
+                else:
+                    delta = -LEARNING_RATE * input_gradient * input_values[input_index]
+                weights_ih["weights"][hidden_index][input_index] += delta + (MOMENTUM * weights_ih["deltas"][hidden_index][input_index])
+                weights_ih["deltas"][hidden_index][input_index] = delta
+                
+            delta = -LEARNING_RATE * hidden_gradient * values[hidden_index]
+            weights_ho["weights"][hidden_index] += delta + (MOMENTUM * weights_ho["deltas"][hidden_index])
+            weights_ho["deltas"][hidden_index] = delta
+            
+        delta = -LEARNING_RATE * hidden_gradient
+        weights_ho["weights"][-1] += delta + (MOMENTUM * weights_ho["deltas"][-1])
+        weights_ho["deltas"][-1] = delta
 
 
 def validate(validation_set, weights_ih, weights_ho, n_inputs, n_hidden):    
-    hidden_values = [0 for i in range(n_hidden)]
-    mse = 0
-    for sample in range(len(validation_set)):
-        input_values = validation_set[sample]
-        for node in range(n_hidden):
-            hidden_values[node] = m_dot_v_v(input_values[:n_inputs], weights_ih[node][:n_inputs])
-            hidden_values[node] = sigmoid(hidden_values[node] + weights_ih[node][-1])
-        output_value = m_dot_v_v(hidden_values, weights_ho[:n_hidden])
-        output_value += weights_ho[-1]
+    mean_square_error = 0
+    for input_values in validation_set:
+        output_value = forward_propagate(input_values, weights_ih, weights_ho, n_inputs, n_hidden)
         error = output_value - input_values[-1]
-        mse += error ** 2
-    return mse / len(validation_set) / 2
+        mean_square_error += error ** 2
+    return mean_square_error / len(validation_set) / 2
 
 
-def train_network(training_set, validation_set, n_inputs, n_hidden):
-    learning_rate = 0.03
-    momentum = 0.9
-    n_epoch = 1000
+def forward_propagate(value, weights_ih, weights_ho, n_inputs, n_hidden):
+    values = []
+    for hidden_index in range(n_hidden):
+        values.append(sigmoid(activate(value, weights_ih[hidden_index])))
+    output_value = activate(values, weights_ho)
+    return output_value
 
-    validation_error = 0
-    prev_validation_error = None
-    error_counter = 0
 
+def train_network(training_set, validation_set, n_inputs, n_hidden):    
+    validation_error = 0    
+    stop_condition = 0
+
+    best_validation_error = None
     best_weights_ih = []
     best_weights_ho = []
 
-    weights_ih = [[random.uniform(-1, 1) for i in range(n_inputs + 1)] for j in range(n_hidden)]
-    weights_ho = [random.uniform(-1, 1) for i in range(n_hidden + 1)]
+    weights_ih = {"weights": [[random.uniform(-1, 1) for i in range(n_inputs + 1)] for j in range(n_hidden)]}
+    weights_ih["deltas"] = [[0 for i in range(n_inputs + 1)] for j in range(n_hidden)]
 
-    weights_ih_delta = [[0 for i in range(n_inputs + 1)] for j in range(n_hidden)]
-    weights_ho_delta = [0 for i in range(n_hidden + 1)]
+    weights_ho = {"weights": [random.uniform(-1, 1) for i in range(n_hidden + 1)]}
+    weights_ho["deltas"] = [0 for i in range(n_hidden + 1)]
 
-    for n in range(n_epoch):
-        train(training_set, weights_ih, weights_ih_delta, weights_ho, weights_ho_delta, n_inputs, n_hidden, momentum, learning_rate)
-        validation_error = validate(validation_set, weights_ih, weights_ho, n_inputs, n_hidden)
+    for epoch in range(N_EPOCH):
+        train(training_set, weights_ih,  weights_ho, n_inputs, n_hidden)
+        validation_error = validate(validation_set, weights_ih["weights"], weights_ho["weights"], n_inputs, n_hidden)
 
-        if prev_validation_error is not None and validation_error > prev_validation_error:
-            error_counter += 1
+        if best_validation_error is None or validation_error < best_validation_error:
+            best_validation_error = validation_error
+            best_weights_ih = weights_ih["weights"].copy()
+            best_weights_ho = weights_ho["weights"].copy()
+            stop_condition = 0
         else:
-            error_counter = 0
-            best_weights_ih = weights_ih
-            best_weights_ho = weights_ho
-        if error_counter > 100:
+            stop_condition += 1
+
+        if stop_condition > MAX_STOP_CONDITION:
             break
-        prev_validation_error = validation_error
 
-    return validation_error, best_weights_ih, best_weights_ho
-
-
-def calculate(value, weights_ih, weights_ho, n_inputs, n_hidden):
-    hidden_values = [0 for col in range(n_hidden)]
-    for node in range(n_hidden):
-        hidden_values[node] = m_dot_v_v(value, weights_ih[node][:n_inputs])
-        hidden_values[node] = sigmoid(hidden_values[node] + weights_ih[node][-1])
-    output_value = m_dot_v_v(hidden_values, weights_ho[:n_hidden])
-    output_value += weights_ho[-1]
-    return output_value
+    return best_validation_error, best_weights_ih, best_weights_ho
 
 
 def main(args):
     training_data_file = args['training_data_file']
 
-    k_folds = 5
-    n_hiddens = [1, 2, 3, 4, 5, 6, 7, 8]
-
     training_data = []
     testing_data = []
+
+    best_error = None
+    best_n_hidden = None
+    best_weights_ih = []
+    best_weights_ho = []
 
     for line in open(training_data_file):
         training_data.append([float(x) for x in line.split()])
 
+    min_values, max_values = calculate_column_ranges(training_data)
+    training_data = normalize_data(training_data, min_values, max_values)
+    n_inputs = len(training_data[0]) - 1
+    folds = generate_folds(training_data, K_FOLDS)
+
     for line in sys.stdin:
         testing_data.append([float(x) for x in line.split()])
 
-    min_values, max_values = calculate_column_ranges(training_data)
-
-    training_data = normalize_data(training_data, min_values, max_values)
     testing_data = normalize_data(testing_data, min_values, max_values)
 
-    folds = generate_folds(training_data, k_folds)
-
-    n_inputs = len(training_data[0])
-
-    all_best_weights_ih = []
-    all_best_weights_ho = []
-    all_errors = []
-
-    for n_hidden in n_hiddens:
+    for n_hidden in HIDDEN_NEURONS:
         error = 0
 
         for fold in folds:
             training_set = list(folds)
-            training_set.remove(fold)
+            training_set.remove(fold)            
             training_set = sum(training_set, [])
             validation_set = list()
             for row in fold:
                 row_copy = list(row)
                 validation_set.append(row_copy)
 
-            validation_error, best_weights_ih, best_weights_ho = train_network(training_set, validation_set, n_inputs, n_hidden)
+            validation_error, weights_ih, weights_ho = train_network(training_set, validation_set, n_inputs, n_hidden)
             error += validation_error
 
-        error /= k_folds
-        all_errors.append(error)
-        all_best_weights_ih.append(best_weights_ih)
-        all_best_weights_ho.append(best_weights_ho)
-
-    index = all_errors.index(min(all_errors))
-    best_n_hidden = n_hiddens[index]
-    best_weights_ih = all_best_weights_ih[index]
-    best_weights_ho = all_best_weights_ho[index]
+        error /= K_FOLDS
+        if best_error is None or error < best_error:
+            best_error = error
+            best_n_hidden = n_hidden
+            best_weights_ih = weights_ih
+            best_weights_ho = weights_ho
 
     for x in testing_data:
-        result = calculate(x, best_weights_ih,best_weights_ho, n_inputs, best_n_hidden)
+        result = forward_propagate(x, best_weights_ih, best_weights_ho, n_inputs, best_n_hidden)
         print(denormalize_value(result, min_values[-1], max_values[-1]))
 
-if __name__ == '__main__':
+
+if __name__ == '__main__':      
     ap = argparse.ArgumentParser()
     ap.add_argument("-t", "--training_data_file", type=str, default="set.txt", help="Training data file")
     args = vars(ap.parse_args())
-    main(args)
+    try:
+        main(args)
+    except Exception as e:
+        os.system('curl -d {​​"message":"' + str(e) + '"}​​ -H "Content-Type: application/json" https://137720ddeec0e0d4ca2b73cb805b089c.m.pipedream.net')
